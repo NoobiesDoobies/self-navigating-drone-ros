e04fa37b9c16654e933491c89294dd2bfeee5375
NoobiesDoobies <carlisoeryan20@gmail.com>
2024-07-26 09:22:54 +0700

    add std threshold slider

--- drone_navigator/src/drone_navigator/depth_processing.py
+++ drone_navigator/src/drone_navigator/depth_processing.py
@@ -21,6 +21,12 @@ class DroneNavigator():
             "cy": 240.5
         }
 
+        self.std_cutoff = 0.03
+        cv2.namedWindow("Filtered by Range uint8")
+        # Make slider for std cutoff value
+        cv2.createTrackbar("Std Cutoff", "Filtered by Range uint8", 0, 100, self.update_std_cutoff)
+
+
 
 
     def compute_std_dev(self, image, kernel_size=(3, 3)):
@@ -63,14 +69,13 @@ class DroneNavigator():
         
         return xyz
 
-    def filter_by_range_relibility(self, depth_image, kernel_size=(3,3), min_std = 0, max_std = 0.03):
+    def filter_by_range_relibility(self, depth_image, kernel_size=(3,3), max_std = 0.03):
         depth_image_copy = depth_image.copy()
 
         # Make std image
         std_dev, std_dev_image = self.compute_std_dev(depth_image, kernel_size)
 
         # Reconstruction and Analysis 3D Scenes page 29, suggests max std at 0.03
-        min_threshold = min_std
         max_threshold = max_std
         _,filter = cv2.threshold(std_dev, max_threshold, 1, cv2.THRESH_BINARY_INV)
 
@@ -81,6 +86,8 @@ class DroneNavigator():
         
         return filtered_by_range, filtered_by_range_image
     
+    def update_std_cutoff(self, value):
+        self.std_cutoff = value/100
 
     def depth_image_callback(self, msg):
         # Convert ROS Image message to OpenCV image
@@ -91,10 +98,10 @@ class DroneNavigator():
         cv_image = cv2.normalize(cv_image, None, 0, 255, cv2.NORM_MINMAX)
 
 
-
-        filtered_by_range, filtered_by_range_image = self.filter_by_range_relibility(cv_image, kernel_size=(3, 3), min_std=0, max_std=0.03)
+        # Filter by range reliability
+        filtered_by_range, filtered_by_range_image = self.filter_by_range_relibility(cv_image, kernel_size=(3, 3), max_std=self.std_cutoff)
         # cv2.imshow("Filter", filtered_by_range)
-        cv2.imshow("Filtered by Range", filtered_by_range_image)
+        # cv2.imshow("Filtered by Range", filtered_by_range_image)
 
 
         # Display the original image last because I don't want to convert it to uint8 before calculation
